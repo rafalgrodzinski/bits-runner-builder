0e62b4ed501593f363fbd9d8935a4af5908793de
Rafał Grodziński <rafal.grodzinski@gmail.com>
2025-06-04 11:08:26 +0900

    Buildable on ARM macOS

--- make.sh
+++ make.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-cc -g -std=c++17 -lc++ -lllvm -DLLVM_DISABLE_ABI_BREAKING_CHECKS_ENFORCING=1 -L/usr/local/opt/llvm/lib -I/usr/local/opt/llvm/include src/*.cpp -o brb
+IFLAGS="-I/usr/local/opt/llvm/include -I/opt/homebrew/opt/llvm/include"
+LDFLAGS="-L/usr/local/opt/llvm/lib -L/opt/homebrew/opt/llvm/lib"
+
+cc -g -std=c++17 -lc++ -lllvm -DLLVM_DISABLE_ABI_BREAKING_CHECKS_ENFORCING=1 ${LDFLAGS} ${IFLAGS} src/*.cpp -o brb
