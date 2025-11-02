86164cd9fc3cea85f3e294a07309b7133fbc87c0
Rafał Grodziński <rafal.grodzinski@gmail.com>
2025-11-02 23:23:34 +0900

    Run all samples at once

--- samples/multi_module/build.sh
+++ samples/multi_module/build.sh
@@ -4,5 +4,5 @@ SCRIPT_PATH="$(readlink -f "${BASH_SOURCE}")"
 SCRIPT_DIR="$(dirname "${SCRIPT_PATH}")"
 BRB_PATH="${SCRIPT_DIR}/../../build/brb"
 
-"${BRB_PATH}" -v "${SCRIPT_DIR}/io.brc" "${SCRIPT_DIR}/stuff.brc" "${SCRIPT_DIR}/main.brc" &&
+"${BRB_PATH}" "${SCRIPT_DIR}/io.brc" "${SCRIPT_DIR}/stuff.brc" "${SCRIPT_DIR}/main.brc" &&
 cc -o multi_module io.o stuff.o main.o
