4b805cb648db4065e6feaf5a79b78e40bed0ee3c
Rafał Grodziński <rafal.grodzinski@gmail.com>
2026-01-06 22:40:29 +0900

    Fixed casting to a a bit

--- src/Lexer/Lexer.cpp
+++ src/Lexer/Lexer.cpp
@@ -529,11 +529,12 @@ shared_ptr<Token> Lexer::matchIdentifier() {
 
     // Special case for misplaced type tokens
     if (
+        lexme.compare("bool") == 0 ||
         lexme.compare("u8") == 0 || lexme.compare("u32") == 0 || lexme.compare("u64") == 0 ||
         lexme.compare("s8") == 0 || lexme.compare("s32") == 0 || lexme.compare("s64") == 0 ||
         lexme.compare("f32") == 0 || lexme.compare("f64") == 0 ||
-        lexme.compare("bool") == 0 ||
-        lexme.compare("data") == 0 || lexme.compare("blob") == 0 || lexme.compare("ptr") == 0
+        lexme.compare("a") == 0 || lexme.compare("ptr") == 0 ||
+        lexme.compare("data") == 0 || lexme.compare("blob") == 0
     ){
         shared_ptr<Token> token = make_shared<Token>(TokenKind::TYPE, lexme, make_shared<Location>(currentFileName, currentLine, currentColumn));
         advanceWithToken(token);
