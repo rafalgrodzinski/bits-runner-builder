d4a01bd0e0d18ab26232cb4ee19382411a2fd99e
Rafał Grodziński <rafal.grodzinski@gmail.com>
2026-05-09 14:29:38 +0900

    Updated library location

--- samples/dyn_array/build.sh
+++ samples/dyn_array/build.sh
@@ -3,5 +3,5 @@
 SCRIPT_PATH="$(readlink -f "${BASH_SOURCE}")"
 SCRIPT_DIR="$(dirname "${SCRIPT_PATH}")"
 
-brb "${SCRIPT_DIR}/main.brc" "${SCRIPT_DIR}/../../lib/B/String.brc" "${SCRIPT_DIR}/../../lib/B/Array.brc" "${SCRIPT_DIR}/../../lib/B/BSys.brc" &&
+brb "${SCRIPT_DIR}/main.brc" "${SCRIPT_DIR}/../../lib/B/String.brc" "${SCRIPT_DIR}/../../lib/B/Array.brc" "${SCRIPT_DIR}/../../lib/BSys.brc" &&
 cc -o dyn_array main.o B.o BSys.o
--- samples/interfaces/build.sh
+++ samples/interfaces/build.sh
@@ -3,5 +3,5 @@
 SCRIPT_PATH="$(readlink -f "${BASH_SOURCE}")"
 SCRIPT_DIR="$(dirname "${SCRIPT_PATH}")"
 
-brb "${SCRIPT_DIR}/main.brc" "${SCRIPT_DIR}/Lib.brc" "${SCRIPT_DIR}/../../lib/B/String.brc" "${SCRIPT_DIR}/../../lib/B/BSys.brc" &&
+brb "${SCRIPT_DIR}/main.brc" "${SCRIPT_DIR}/Lib.brc" "${SCRIPT_DIR}/../../lib/B/String.brc" "${SCRIPT_DIR}/../../lib/BSys.brc" &&
 cc -o interfaces main.o Lib.o B.o BSys.o
