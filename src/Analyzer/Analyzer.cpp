f4465b46fe7afee4f5c804a519b31596bf453b8f
Rafał Grodziński <rafal.grodzinski@gmail.com>
2026-09-16 22:51:57 +0900

    Better handling

--- src/Analyzer/Analyzer.cpp
+++ src/Analyzer/Analyzer.cpp
@@ -931,14 +931,6 @@ shared_ptr<ValueType> Analyzer::typeForExpression(shared_ptr<ExpressionCast> exp
             );
         }
         return expressionCast->getValueType();
-    } else if (isTargetPointer && !isSourceComposite) {
-        shared_ptr<ValueTypePtr> ptrValueType =  expressionCast->getValueType()->toPtr();
-        if (ptrValueType->getPointeeValueType() == nullptr) {
-            ptrValueType->pointeeValueType = parentExpression->getValueType();
-            return ptrValueType;
-        } else if (ptrValueType->getPointeeValueType()->isEqual(parentExpression->getValueType())) {
-            return ptrValueType;
-        }
     // cast composite to complex type
     } else if (isSourceComposite && (isTargetBlob || isTargetData || isTargetEnumField || isTargetPointer || isTargetProto)) {
         if (canImplicitCast(parentExpression->getValueType(), expressionCast->getValueType())) {
@@ -958,6 +950,14 @@ shared_ptr<ValueType> Analyzer::typeForExpression(shared_ptr<ExpressionCast> exp
     } else if (isSourceBlob && isTargetBlob) {
         if (parentExpression->getValueType()->toBlob()->getSymbolName()->isEqual(expressionCast->getValueType()->toBlob()->getSymbolName()))
             return expressionCast->getValueType();
+    }  else if (isTargetPointer && !isSourceComposite) {
+        shared_ptr<ValueTypePtr> ptrValueType =  expressionCast->getValueType()->toPtr();
+        if (ptrValueType->getPointeeValueType() == nullptr) {
+            ptrValueType->pointeeValueType = parentExpression->getValueType();
+            return ptrValueType;
+        } else if (ptrValueType->getPointeeValueType()->isEqual(parentExpression->getValueType())) {
+            return ptrValueType;
+        }
     }
 
     markErrorInvalidCast(expressionCast->getLocation(), parentExpression->getValueType(), expressionCast->getValueType());
